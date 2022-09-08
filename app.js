b70eb64b4fd167fcd88d8b00886f56d74902bfce
Ahmed Hussein <ahmed_ali467@icloud.com>
2022-09-07 20:16:25 -0700

    checked console log

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const nameDisplay = document.getElementById('name-display');
+console.log(nameDisplay);
 
 const nameInput = document.getElementById('name-input');
+console.log(nameInput);
 
 const changeButton = document.getElementById('change-name');
+console.log(changeButton);
 
 changeButton.addEventListener('click', () => {
     nameDisplay.textContent = nameInput.value;
