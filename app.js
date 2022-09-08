07e04ed969470bcf33366fef1fa3152324e07eb3
Ahmed Hussein <ahmed_ali467@icloud.com>
2022-09-07 20:28:33 -0700

    extraneous console.log statements

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 const nameDisplay = document.getElementById('name-display');
-console.log(nameDisplay);
-
 const nameInput = document.getElementById('name-input');
-console.log(nameInput);
-
 const changeButton = document.getElementById('change-name');
-console.log(changeButton);
-
 changeButton.addEventListener('click', () => {
-    console.log('click');
     nameDisplay.textContent = nameInput.value;
     nameInput.value = '';
 });
